413d10e598a4485250e7c8dce9a3268b7bcf5c51
repl.it user <replituser@example.com>
2021-02-27 22:12:02 +0000

    :open_file_folder:Even Fibonacci Numbers

--- problem2.js
+++ problem2.js
@@ -5,7 +5,21 @@
 // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 // By considering the terms in the Fibonacci sequence whose values do not exceed n, find the sum of the even-valued terms.
 
-function fiboEvenSum(n) {
+exports.fiboEvenSum= function(number) {
+  if (number <= 1) {
+    return 0;
+  } else {
+    let evenSum = 0,
+      prevFibNum = 1,
+      // According to problem description our Fibonacci series starts with 1, 2
+      fibNum = 2; 
+    for (let i = 2; fibNum <= number; i++) {
+      if (fibNum % 2 == 0) {
+        evenSum += fibNum;
+      }
+      [prevFibNum, fibNum] = [fibNum, prevFibNum + fibNum];
+    }
+    return evenSum;
+  }
+};
 
-  return true;
-}
