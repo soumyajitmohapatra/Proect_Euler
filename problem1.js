03120c7103e7a3c4c2fd13e4b9e29e67bdf26f36
repl.it user <replituser@example.com>
2021-02-27 20:37:42 +0000

    :open_file_folder: Multiples of 3 and 5

--- problem1.js
+++ problem1.js
@@ -1,4 +1,4 @@
-        //  Problem 1: Multiples of 3 and 5Passed
+//      Problem 1: Multiples of 3 and 5
 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
@@ -13,5 +13,4 @@ const multiplesOf3and5 =(number)=>{
   }
   return sum;
 }
-
 multiplesOf3and5(1000);
