3ab82f5619b658bb42117221ceca986480458386
repl.it user <replituser@example.com>
2021-02-28 16:26:04 +0000

    :zap: Repl.it index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ p1.multiplesOf3and5(100) //2318
 
 const p2 = require("./problem2")
 p2.fiboEvenSum(60) //44 
+
+const p3 = require("./problem3")
+p3.largestPrimeFactor(13195 ); //29
+
+const p4 = require("./problem4")
+p4.largestPalindromeProduct(3); //906609
