528028117e6ec7cf200c5a659a50cea19818bc49
repl.it user <replituser@example.com>
2021-02-28 16:21:42 +0000

    :open_file_folder: Largest prime factorPassed

---
+++ problem3.js
@@ -0,0 +1,18 @@
+//           Problem 3: Largest prime factorPassed
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the given number?
+
+exports.largestPrimeFactor = function (n) {
+  let i=2;
+    while (i<=n){
+    if (n%i == 0){
+        n/=i;    
+    }else{
+        i++;
+    }
+}
+  return i;
+}
+
