9b59176bd6fa4a0121f4d438846eade334f475e8
repl.it user <replituser@example.com>
2021-03-04 19:02:24 +0000

    open_file_folder: Power digit sum

---
+++ problem16.js
@@ -0,0 +1,16 @@
+//         Problem 16: Power digit sum
+
+// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+// What is the sum of the digits of the number 2^exponent ?
+
+
+exports.powerDigitSum = function(exponent) {
+
+  const num = BigInt(Math.pow(2, exponent))
+  let arr = num.toString().split('')
+  arr.slice(arr.length - 1)
+
+  return arr.reduce((r, c) => parseInt(r) + parseInt(c))
+}
+
